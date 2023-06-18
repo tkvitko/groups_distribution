f8260c35a2a0133f5ee24ddbd4d8620f9d10d3f7
Taras Kvitko <tkvitko@gmail.com>
2023-06-18 22:30:07 +0300

    bugfix in file removing for windows

--- main_multiprocessing.py
+++ main_multiprocessing.py
@@ -1,17 +1,11 @@
 import concurrent.futures
-import os
 
-from main import (Data,
-                  config,
-                    WORK_DIR,
-                  SOURCE_FILENAME,
-                  RESULT_FILENAME,
-                  REMOVED_ITEMS_FILENAME)
+from main import *
 
 TEMP_FILE = 'processes_cache'
 
 
-def job(number):
+def job(job_number: int):
     data = Data(source_file=SOURCE_FILENAME)
 
     while data.has_group_with_rating_less_then_requested:
@@ -19,10 +13,10 @@ def job(number):
         for i in range(int(config['shuffler']['tries_number'])):
             data.shuffle_and_get_quality()
             if not data.has_group_with_rating_less_then_requested:
-                data.save_data_to_excel(number)
+                data.save_data_to_excel(job_number)
                 deviation = data.get_max_feedback_count_deviation()
                 with open(TEMP_FILE, 'a', encoding='utf-8') as f:
-                    f.write(f'{number}:{deviation}\n')
+                    f.write(f'{job_number}:{deviation}\n')
                 break
         else:
             data.remove_the_worst_item()
@@ -36,24 +30,32 @@ if __name__ == '__main__':
         for future in concurrent.futures.as_completed(futures_dict):
             future.result()
 
+    # чтение результатов работы
     with open(TEMP_FILE, encoding='utf-8') as f:
         results = f.readlines()
 
+    # поиск лучшего результата
     min_deviation = 100
     best_number = None
     for result in results:
+        print(result)
         number, deviation = result.split(":")
         deviation = float(deviation)
         if min_deviation > deviation:
             min_deviation = deviation
             best_number = number
 
-    for file in os.listdir(WORK_DIR):
-        for name in (RESULT_FILENAME.split('/')[-1], REMOVED_ITEMS_FILENAME.split('/')[-1]):
-            if name in file and best_number not in file:
-                os.remove(os.path.join(WORK_DIR, file))
-                print(f'{file} removed')
-
     os.remove(TEMP_FILE)
-    print(f'{TEMP_FILE} removed')
+    print(f'removed: {TEMP_FILE}')
 
+    # удаление всех результатов, кроме лучшего
+    os.chdir(WORK_DIR)
+    for file in os.listdir('.'):
+        for name in (os.path.split(RESULT_FILENAME)[-1], os.path.split(REMOVED_ITEMS_FILENAME)[-1]):
+            if name in file:
+                if best_number not in file:
+                    os.remove(file)
+                    print(f'removed: {file}')
+                else:
+                    os.rename(file, file.replace(best_number, ''))
+                    print(f'renamed: {file}')
